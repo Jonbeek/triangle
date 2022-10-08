c01ee7d2e204564d32a2a9d0897dba0f4be9d3e0
Jonbeek <jonbeek@gmail.com>
2022-10-08 12:01:34 -0400

    Complete fork

--- cmd/triangle/main.go
+++ cmd/triangle/main.go
@@ -21,8 +21,8 @@ import (
 	"text/template"
 	"time"
 
-	"github.com/esimov/triangle/v2"
-	"github.com/esimov/triangle/v2/utils"
+	"github.com/Jonbeek/triangle/v2"
+	"github.com/Jonbeek/triangle/v2/utils"
 	"golang.org/x/image/bmp"
 	"golang.org/x/term"
 )
